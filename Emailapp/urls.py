eaced31dccac74eaaa9985f542d7d0732b2ca0d9
Anup@453 <aghattikar82@gmail.com>
2025-02-28 15:57:15 +0530

    Added Login Page

--- Emailapp/urls.py
+++ Emailapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import upload_excel, generate_emails, add_company, add_email_format, detect_email_format,upload_and_convert
+from .views import index,upload_excel, generate_emails, add_company, add_email_format, detect_email_format,upload_and_convert
 
 urlpatterns = [
+    path('',index, name='index'),
     path('upload/',upload_and_convert, name='upload_and_convert'),
-    path('', upload_excel, name='upload_excel'),
+    path('upload_excel', upload_excel, name='upload_excel'),
     path('generate-emails/', generate_emails, name='generate_emails'),
     path('add-company/', add_company, name='add_company'),
     path('', add_email_format, name='add_email_format'),
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# autocreation
+autocreation
